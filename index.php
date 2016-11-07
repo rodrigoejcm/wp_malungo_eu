2f975f16d31cd7696cb67a21ff3e90624345583a
Matheus Graciano (pc-beta) <graciano.dev@gmail.com>
2016-11-07 17:18:30 -0200

    home redesign feito

--- index.php
+++ index.php
@@ -57,26 +57,28 @@ get_header(); ?>
 								</div>
 				            </div>
 				            <div class="col-md-4 col-sm-12 col-h-400">
-				            	<a href="" class="hover-blue">
-				            		<p class="categoria cor-atividade">Artes Visuais</h3>
+				            	<a href="" class="hover-artesvisuais">
+				            		<h3 class="categoria cor-artesvisuais">Artes Visuais</h3>
 				               		<h2 class="titulo">Conheça o Land Art com o coletivo Site Specific.</h2>
-				               		<p class="resumo">O coletivo Site_Specific teve início em 2011 como um esforço colaborativo dos sul-africanos Strijdom van der Merwe, Anni Snyman, PC van Rensburg, Heather Greig, Erica Lüttich, entre outros artistas, amigos e simpatizantes. Lançado durante a “International Land Art Biennale” na baía de Plettenberg na África do Sul, o coletivo, devotado à arte na natureza, promove desde então Land Arts em Site Specifics, proporcionando o diálogo entre as práticas culturais no país e os baixos impactos ambientais provocados por tais ações....</p>
+				               		<p class="resumo">O coletivo Site_Specific teve início em 2011 como um esforço colaborativo dos sul-africanos Strijdom van der Merwe, Anni Snyman, PC van Rensburg, Heather Greig, Erica Lüttich, entre outros artistas, amigos e simpatizantes. Lançado durante a “International Land Art Biennale” na baía de Plettenberg na África do Sul, o coletivo, devotado...</p>
 				            	</a>
 					        </div>
 			            </div>
 				    </div>
-				    <br><br><br>
+				    <br>
 				    <div class="row">
 				    	<div class="col-md-6">
+
 				    		<div class="home-post">
 					    		<div class="imagem-h-300">
 						    		<img src="<?=get_template_directory_uri().'/img/DESTAQUE_02.jpg' ?>" >
 									</img>
 								</div>
-								<br><br>
-				            	<p class="categoria cor-malunguices">Dança</h3>
-				               	<h2 class="titulo">Tradição e contemporâneo juntos!.</h2>
-				               	<p class="resumo">O grupo de dança contemporânea cabo-verdiano Raiz di Polon foi fundado em 1991, pelos coreógrafos José Emanuel Brandão (Mano Preto) e Zezinho Semedo. Sua formação é composta ainda pelo diretor musical e compositor Mário Lúcio, pelos bailarinos Carlos Oliveira, Bety Fernandes, Luís da Rosa, Rosy Timas, Djemilson Barreto, e pelo produtor executivo Jeff Hessney...</p>
+								<a href="" class="hover-danca">
+					            	<h3 class="categoria cor-danca">Dança</h3>
+					               	<h2 class="titulo">Tradição e contemporâneo juntos!.</h2>
+					               	<p class="resumo">O grupo de dança contemporânea cabo-verdiano Raiz di Polon foi fundado em 1991, pelos coreógrafos José Emanuel Brandão (Mano Preto) e Zezinho Semedo. Sua formação é composta ainda pelo diretor musical e compositor Mário Lúcio, pelos bailarinos Carlos Oliveira, Bety Fernandes, Luís da Rosa, Rosy Timas, Djemilson Barreto, e pelo produtor executivo Jeff Hessney...</p>
+				               </a>
 				            </div>
 			            </div>
 			            <div class="col-md-6">
@@ -85,10 +87,11 @@ get_header(); ?>
 				               	<img src="<?=get_template_directory_uri().'/img/DESTAQUE_03.png' ?>" >
 								</img>
 							</div>
-								<br><br>
-				            	<p class="categoria cor-sociedade">MÙSICA</h3>
-				               	<h2 class="titulo"> De Moçambique aos festivais de São Paulo.</h2>
-				               	<p class="resumo">A cantora Lenna Bahule nasceu em 1989, em Maputo, e hoje reside no Brasil, na cidade de São Paulo. Sua iniciação musical teve início aos cinco anos, quando ingressou na Escola Nacional de Música (ENM), estudando piano. A carreira como vocalista solo se deu a partir de 2006, quando Lenna passa a compor, participar de festivais, fazer parcerias e experimentações com artistas moçambicanos e de outros países...</p>
+								<a href="" class="hover-musica">
+					            	<h3 class="categoria cor-musica">Música</h3>
+					               	<h2 class="titulo"> De Moçambique aos festivais de São Paulo.</h2>
+					               	<p class="resumo">A cantora Lenna Bahule nasceu em 1989, em Maputo, e hoje reside no Brasil, na cidade de São Paulo. Sua iniciação musical teve início aos cinco anos, quando ingressou na Escola Nacional de Música (ENM), estudando piano. A carreira como vocalista solo se deu a partir de 2006, quando Lenna passa a compor, participar de festivais, fazer parcerias e experimentações com artistas moçambicanos e de outros países...</p>
+				               </a>
 				            </div>
 			            </div>
 				    </div>

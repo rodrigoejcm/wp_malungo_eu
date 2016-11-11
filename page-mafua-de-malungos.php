8335dcc4ba83892b3ab0a1f13dd55f2a80eb5c35
Matheus Graciano (pc-beta) <graciano.dev@gmail.com>
2016-11-11 20:53:38 -0200

    redesign pagina mafua de malungos

--- page-mafua-de-malungos.php
+++ page-mafua-de-malungos.php
@@ -20,19 +20,15 @@ get_header(); ?>
 				<img src="<?= src_img('mafua-banner.jpg')?>">
 				<?= get_svg('titulo-mafuademalungos', true) ?>
 			</div>
+			<div class="mafua-content">
+				<?php
+				while ( have_posts() ) : the_post();
 
-			<?php
-			while ( have_posts() ) : the_post();
+					the_content();
 
-				get_template_part( 'template-parts/content', 'page' );
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-
-			endwhile; // End of the loop.
-			?>
+				endwhile; // End of the loop.
+				?>
+			</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->

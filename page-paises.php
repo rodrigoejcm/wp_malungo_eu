aa367cd017ad19339fa29bc1ec08053df870dff9
Matheus Graciano (pc-beta) <graciano.dev@gmail.com>
2016-11-11 17:46:20 -0200

    pagina paises done

--- page-paises.php
+++ page-paises.php
@@ -17,57 +17,51 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 		<div class="paises-lista">
-			<h1>Países</h1>
-			<p>Explore a morada de nossos Malungos...</p>
+			<h1>
+				<img src="<?=get_template_directory_uri().
+						'/img/titulo-paises.svg'?>"
+					alt="ARTES"
+					/>
+			</h1>
+				<ul class='grid-paises'>
+					<?php
 
-			
-			<div class="row">
-				<div class="col-md-8 col-md-offset-2">
-					<ul>
-
-
-						<?php
-
-							$posts = get_posts(array(
-								'numberposts' => -1,
-								'post_type' => 'pais',
-								'order' => 'ASC',
-								'orderby' => 'title'
-							));
+						$posts = get_posts(array(
+							'numberposts' => -1,
+							'post_type' => 'pais',
+							'order' => 'ASC',
+							'orderby' => 'title'
+						));
 
 
-							if($posts)
+						if($posts)
+						{
+							foreach($posts as $post)
 							{
-								foreach($posts as $post)
-								{
-								?>
-									<li> 
-										<?php
-											$thumb_id = get_post_thumbnail_id();
-											$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
-											$thumb_url = $thumb_url_array[0];
-										?>
+							?>
+								<li> 
+									<?php
+										$thumb_id = get_post_thumbnail_id();
+										$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
+										$thumb_url = $thumb_url_array[0];
+									?>
 
 
 
-										<a  href="<?=get_permalink();?>">
-											<div class="box-pais">
-												<img src="<?php echo get_field('imagem_mapa')['url']; ?>" >
-												</img>
-												<p><?php the_title();?></p>
-											</div>
-										</a>
-									</li>
-								<?php
-								}
+									<a  href="<?=get_permalink();?>" class="link-pais">
+										<div class="box-pais">
+											<img src="<?php echo get_field('imagem_mapa')['url']; ?>" >
+											</img>
+											<p><?php the_title();?></p>
+										</div>
+									</a>
+								</li>
+							<?php
 							}
-					?>
-
-					</ul>
-				</div>
-
+						}
+				?>
+				</ul>
 			</div>
-		</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 

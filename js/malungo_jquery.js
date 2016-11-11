8d8b801d3cc44326b44d101aa7797dc709d738a4
Matheus Graciano (pc-beta) <graciano.dev@gmail.com>
2016-11-11 21:29:29 -0200

    transparência na logo acima + maitree light

--- js/malungo_jquery.js
+++ js/malungo_jquery.js
@@ -18,29 +18,27 @@ $(document).ready(function () {
     if(!isHome || $(document).scrollTop() > 10){
         $('.logo-malungo img').addClass('active');
     }
-    if(isHome){
-        $(document).scroll(function () {
-            var x = $(this).scrollTop(),
-                home = $('.cor-scroll');
+    $(document).scroll(function () {
+        var x = $(this).scrollTop(),
+            home = $('.cor-scroll');
 
+        if(isHome){
             if(x > 10)
                 $('.logo-malungo img').addClass('active');
             else
                 $('.logo-malungo img').removeClass('active');
+        }
 
-            if (x > 40) {
-                $('.cor-scroll').css("background-color", "#303331");
-                $('.home-menu').css("border-bottom", "none");        
-            }else{
-                $('.cor-scroll').css("background-color", "transparent");
-                $('.home-menu').css("border-bottom",
-                                "1px solid rgba(247,239,231,0.5)");
-            }
-            
-        });
-    }
-    else
-        $('.cor-scroll').css("background-color", "#303331");
+        if (x > 40) {
+            $('.cor-scroll').css("background-color", "#303331");
+            $('.home-menu').css("border-bottom", "none");        
+        }else{
+            $('.cor-scroll').css("background-color", "transparent");
+            $('.home-menu').css("border-bottom",
+                            "1px solid rgba(247,239,231,0.5)");
+        }
+        
+    });
 });
     
 

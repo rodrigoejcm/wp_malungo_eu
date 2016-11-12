8a16e34bd261e3ddfffe25c220ae1b3e704e37d4
Matheus Graciano (pc-beta) <graciano.dev@gmail.com>
2016-11-12 16:39:42 -0200

    animação home não-aprovada-ainda

--- js/malungo_jquery.js
+++ js/malungo_jquery.js
@@ -17,23 +17,43 @@ $(document).ready(function () {
     isHome = home_paths.indexOf(location)!=-1;
     if(!isHome || $(document).scrollTop() > 10){
         $('.logo-malungo img').addClass('active');
+        $('.img-yaon').addClass('animation-done');
     }
+    var logoUpLock = false;
+    var logoDownLock = false;
     $(document).scroll(function () {
         var x = $(this).scrollTop(),
             home = $('.cor-scroll');
 
         if(isHome){
-            if(x > 10)
-                $('.logo-malungo img').addClass('active');
-            else
-                $('.logo-malungo img').removeClass('active');
+            if(x > 10){
+                $('.img-yaon').addClass('animation-done');
+                if(!logoUpLock){
+                    logoUpLock = true;
+                    setTimeout(function(){
+                        $('.logo-malungo img').addClass('active');
+                        logoUpLock = false;
+                    }, 300);
+                }
+            }
+            // else{
+            //     $('.logo-malungo img').removeClass('active');
+            //     if(!logoDownLock){
+            //         logoDownLock = true;
+            //         setTimeout(function(){
+            //             $('.img-yaon').removeClass('animation-done');
+            //             logoDownLock = false;
+            //         }, 300);
+            //     }
+            // }
         }
 
         if (x > 40) {
             $('.cor-scroll').css("background-color", "#303331");
             $('.home-menu').css("border-bottom", "none");        
         }else{
-            $('.cor-scroll').css("background-color", "transparent");
+            if(!isHome)
+                $('.cor-scroll').css("background-color", "transparent");
             $('.home-menu').css("border-bottom",
                             "1px solid rgba(247,239,231,0.5)");
         }

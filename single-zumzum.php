ef55c72770e6aeb26dcb332dd1ea5bdce3bce262
Matheus Graciano (pc-beta) <graciano.dev@gmail.com>
2016-12-02 20:04:54 -0200

    fechando post zumzum

--- single-zumzum.php
+++ single-zumzum.php
@@ -21,22 +21,22 @@ get_header();
             ?>
             <h2><?php echo $post->post_title ?></h2>
             <img src="<?= $thumb_url ?>" class="featured-img">
-            <blockquote><?= get_field('olho_de_texto') ?></blockquote>
             <?php echo apply_filters('the_content', $post->post_content); ?>
             <?php echo apply_filters('the_content', get_field('para_escurecer')); ?>
-            <?php
-                $posts = get_posts(array(
-                    'numberposts' => -1 ,
-                    'post_type' => 'artista',
-                    'meta_query' => array(
-                        array(
-                            'key' => 'pais',
-                            'value' => '"' . get_the_ID() . '"',
-                            'compare' => 'LIKE'
-                        )
-                    )
-                ));
-            ?>
+            <?php echo apply_filters('the_content', get_field('referencias')); ?>
+            <div class="autor">
+                <?php
+                    $autor = get_field('autor');
+                    $thumb_id = get_post_thumbnail_id($autor->ID);
+                    $thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
+                    $thumb_url = $thumb_url_array[0];
+                ?>
+                <img src="<?= $thumb_url ?>">
+                <div class="content-autor">
+                    <h3><?= $autor->post_title ?></h3>
+                    <?php echo apply_filters('the_content', $post->post_content); ?>
+                </div>
+            </div>
         </main><!-- #main -->
     </div>
 <?php
